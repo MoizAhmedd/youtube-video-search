901e2491ff420e1589865e1137535b5203a086ad
MoizAhmedd <moizz.ahmed@mail.utoronto.ca>
2021-03-19 18:17:03 -0400

    remove vtt files properly

--- get_transcript.sh
+++ get_transcript.sh
@@ -2,15 +2,11 @@
 # video-cap.sh videoUrl sub.txt
 
 # Download captions only and save in a .vtt file
-youtube-dl --skip-download --write-auto-sub "https://www.youtube.com/watch?v=$1";
+youtube-dl --skip-download --write-auto-sub "https://www.youtube.com/watch?v=$1" -o "$2";
 
-# Find .vtt files in current directory created within last 3 seconds, limit to 1
-vtt=$(ls *.vtt)
+# # Extract the subs and save as plaintext, removing time, new lines and other markup
+sed '1,/^$/d' "$2.en.vtt" | sed 's/<[^>]*>//g' | awk -F. 'NR%8==1{printf"%s ",$1}NR%8==3' > "transcript_$2.txt"
 
-# Extract the subs and save as plaintext, removing time, new lines and other markup
-sed '1,/^$/d' "$vtt" | sed 's/<[^>]*>//g' | awk -F. 'NR%8==1{printf"%s ",$1}NR%8==3' > "transcript_$2.txt"
-
-# Remove the original .vtt subs file
-rm -f "$vtt"
-
+# # Remove the original .vtt subs file
+rm -f "$2.en.vtt"
 
